579ddf55df548ec38dc9601d1768436bc4a560c0
lpiedras4 <piedras.leo@gmail.com>
2025-07-18 22:25:19 -0700

    added tailwind files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-import './globals.css';
+import './index.css';
+import {Routes, Route} from 'react-router-dom';
 const App = () => {
   return (
-    <div>App</div>
+    <main className='flex h-screen'> 
+            App
+    </main>
+   
   );
 }
 
