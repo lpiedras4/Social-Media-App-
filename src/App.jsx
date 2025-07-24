de039a163d1aeb00fccae2d6ff49c0e68d44f51c
lpiedras4 <piedras.leo@gmail.com>
2025-07-23 20:45:46 -0700

    Added files and folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-import './index.css';
 import {Routes, Route} from 'react-router-dom';
+import './index.css';
+import SigninForm from './_auth/forms/SigninForm';
+import {Home} from './_root/pages';
+import SignupForm from './_auth/forms/SignupForm';
+import RootLayout from './_root/RootLayout';
+import AuthLayout from './_auth/AuthLayout';
 const App = () => {
   return (
     <main className='flex h-screen'> 
-            App
+    <Routes>
+    {/*public routes*/}
+    <Route element={<AuthLayout />}>
+      <Route path = "/sign-in" element={<SigninForm/>} />
+      <Route path = "/sign-up" element={<SignupForm/>} />
+    </Route>
+
+      {/*private routes*/}
+      <Route element={<RootLayout/>}>
+         <Route index element={<Home/>}/>
+      </Route>
+    </Routes>
+   
+    
     </main>
    
   );
