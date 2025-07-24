af93a1527f52cfb843dd31aa19e16e2123d6ec5a
lpiedras4 <piedras.leo@gmail.com>
2025-07-23 21:41:44 -0700

    Added auth template for tailwind

--- src/_auth/AuthLayout.jsx
+++ src/_auth/AuthLayout.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import SigninForm from './forms/SigninForm';
+import {Outlet} from 'react-router-dom';
 const AuthLayout = () => {
   return (
-    <div>AuthLayout</div>
+   <>
+          <section className=" bg-black flex flex-1 justify-center items-center flex-col py-10" >
+            <SigninForm/>
+          </section>
+          <img
+            src="https://www.shoptezza.com/cdn/shop/products/IMG_0777.jpg?v=1678158646"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
+        </>
+
   )
 }
 
--- src/_auth/forms/SigninForm.jsx
+++ src/_auth/forms/SigninForm.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 const SigninForm = () => {
   return (
-    <div>SigninForm</div>
+     <form>
+                         <div>
+                            <div className = "grid-columns-3 mb-8">
+                            <h2 className="text-white text-center text-5xl"> Sign in to your account</h2>
+                            <p className="text-white text-lg my-4 text-center">
+                            Welcome back! Please enter your details.
+                            </p>
+                            </div>
+                            <label for="email" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email Address</label>
+                            <input type="email" name="email" id="email" placeholder="example@example.com" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                        </div>
+
+                        <div class="mt-6">
+                            <div class="flex justify-between mb-2">
+                                <label for="password" class="text-sm text-gray-600 dark:text-gray-200">Password</label>
+                                <a href="#" class="text-sm text-gray-400 focus:text-blue-500 hover:text-blue-500 hover:underline">Forgot password?</a>
+                            </div>
+
+                            <input type="password" name="password" id="password" placeholder="Your Password" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                        </div>
+
+                        <div class="mt-6">
+                            <button class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                                Sign in
+                            </button>
+                        </div>
+
+     </form>
   )
 }
 
