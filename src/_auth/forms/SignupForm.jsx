bb339cff80383a22729579bddbe86488079af94c
lpiedras4 <piedras.leo@gmail.com>
2025-07-29 20:12:08 -0700

    había puesto el metodo de handle submit en el label en lugar del input lol

--- src/_auth/forms/SignupForm.jsx
+++ src/_auth/forms/SignupForm.jsx
@@ -3,9 +3,9 @@ import {useState} from 'react';
 import { Link } from 'react-router-dom';
 import supabase from '../../helper/supabase.client';
 const SignupForm = () => {
-  const[email,setEmail ] = useState("");
+  const[email, setEmail] = useState("");
   const[password, setPassword] = useState("");  
-  const [message,setMessage] = useState("");
+  const [message, setMessage] = useState("");
   const [username, setUsername] = useState("");
   const [name, setName] = useState("");
   const [lastname, setLastName] = useState("");
@@ -18,7 +18,7 @@ const handleSubmit = async (event) =>{
         lastname:lastname,
         email:email,
         password:password
-    });
+    }); 
     if(error){
         setMessage(error.message);
         return;
@@ -27,11 +27,12 @@ const handleSubmit = async (event) =>{
         setMessage("Your account has been created succesfully!");
 
     }
-    setName("");
     setUsername("");
+    setName("");
+    setLastName("");
     setEmail("");
     setPassword("");
-    setLastName("");
+    
 };
   return (
     <>
@@ -66,39 +67,42 @@ const handleSubmit = async (event) =>{
                             </div>
                             <div className="mt-6">
                             <label 
+                            for="lastname" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Last name</label>
+                            <input 
                             onChange={
                                 (e) => setLastName(e.target.value)
                             } 
                             value={lastname}
                             required
-                            for="lastname" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Last name</label>
-                            <input type="lastname" name="lastname" id="lastname" placeholder="Enter your last name" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                            type="lastname" name="lastname" id="lastname" placeholder="Enter your last name" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                             </div>
                             <div class="mt-6">
                             <label
-                            onChange={
+                            for="email" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email Address</label>
+                            <input 
+                             onChange={
                                 (e) => setEmail(e.target.value)
                             } 
                             value={email}
                             required
-                            for="email" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email Address</label>
-                            <input type="email" name="email" id="email" placeholder="Register an email" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                            type="email" name="email" id="email" placeholder="Register an email" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                             </div>
                         </div>
 
                         <div class="mt-6">
                             <div class="flex justify-between mb-2">
-                                <label 
-                                onChange={
-                                (e) => setPassword(e.target.value)
-                            } 
-                            value={password}
-                            required    
+                                <label   
                                 for="password" class="text-sm text-gray-600 dark:text-gray-200">Password</label>
                                 <a href="#" class="text-sm text-gray-400 focus:text-blue-500 hover:text-blue-500 hover:underline">Forgot password?</a>
                             </div>
 
-                            <input type="password" name="password" id="password" placeholder="Create a password" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                            <input 
+                            onChange={
+                                (e) => setPassword(e.target.value)
+                            } 
+                            value={password}
+                            required  
+                            type="password" name="password" id="password" placeholder="Create a password" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                         </div>
 
                         <div class="mt-6">
