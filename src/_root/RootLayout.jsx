b6a2416fcdd11d34c6db9e100acad3a8a6ae5328
lpiedras4 <piedras.leo@gmail.com>
2025-08-09 14:08:49 -0700

    Added signout and profile icons

--- src/_root/RootLayout.jsx
+++ src/_root/RootLayout.jsx
@@ -6,10 +6,11 @@ import Bottombar from './pages/components/Bottombar';
 const RootLayout = () => {
    
   return (
-
     <div className='w-full md:flex bg-slate-950'> 
       <TopBar />
+      <div className="md:flex">
       <LeftSidebar/>
+      </div>
       <section className="flex flex-1 h-full">
         <Outlet />
       </section>
--- src/_root/pages/components/TopBar.jsx
+++ src/_root/pages/components/TopBar.jsx
@@ -2,16 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import supabase from '../../../helper/supabase.client';
 import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { FaUserCircle } from "react-icons/fa";
+import { IoIosLogOut } from "react-icons/io";
 const TopBar = () => {
     const navigate = useNavigate();
+    const [userId,setUserId] = useState(null);
+    useEffect(()=>{
+      const getUser =  async () => {
+        const{data} = await supabase.auth.getUser();
+        setUserId(data?.user.id || null);
+        
+      };
+      getUser();
+    },[]);
   const signOut = async () => {
     const {error} = await  supabase.auth.signOut();
    if(error)throw error;
    navigate("/sign-in");
   };
   return (
-  <section className="topbar">
-    <div className="flex-between py-4 px-5">
+  <section className="topbar w-full">
+    <div className="flex justify-between items-center py-4 px-5 w-full">
       <Link to="/" className="flex gap-3 items-center">
         <img className="w-50 h-full" 
         src="/images/logo.svg"
@@ -20,11 +32,19 @@ const TopBar = () => {
         height={325}
         />
       </Link>
-      <div className="flex gap-4">
-      <button variant="ghost" className="shad-button_ghost" onClick={signOut}>
-        <img src="/images/logout.svg"/>
-      </button>
+      <div className="flex items-center gap-4"> 
+            <button variant="ghost" className="shad-button_ghost" onClick={signOut}>
+            <IoIosLogOut className="w-10 h-full text-white"/>
+          </button>
+          {userId && ( 
+            <Link to={`/profile/${userId}`} className="text-white ml-4">
+            <FaUserCircle className="w-10 h-full"/>
+            </Link>
+        )}
       </div>
+      
+     
+      
     </div>
   </section>
   )
