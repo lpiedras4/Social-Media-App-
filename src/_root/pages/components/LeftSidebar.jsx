bb5964a2a7c36930a78ef03f4b051a9bc1d6ca2f
lpiedras4 <piedras.leo@gmail.com>
2025-08-14 20:47:29 -0700

    Started implementing leftsidebar and added home and profile buttons

--- src/_root/pages/components/LeftSidebar.jsx
+++ src/_root/pages/components/LeftSidebar.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
+import { BiHomeSmile } from "react-icons/bi";
+import supabase from '../../../helper/supabase.client';
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { FaUserCircle } from "react-icons/fa";
 const LeftSidebar = () => {
+    const navigate = useNavigate();
+    const [userId,setUserId] = useState(null);
+    const[username,setUsername] = useState("");
+    const [name,setName] = useState("");
+    const [lastName,setLastName] = useState("");
+    useEffect(()=>{
+      const getUserData =  async () => {
+        const{data:userData} = await supabase.auth.getUser();
+        const id = userData?.user?.id || null
+        setUserId(id);
+
+        if(id){
+          const {data:profileData} = await supabase
+          .from("profiles")
+          .select("*")
+          .eq("id", id)
+          .single();
+          setUsername(profileData?.username || "");
+          setName(profileData?.name || "");
+          setLastName(profileData?.lastname || "");
+        }
+      };
+      getUserData();
+    },[]);
   return (
-    <div>LeftSidebar</div>
+    <nav className="leftsidebar"> 
+      <div className="flex flex-col gap-10">
+        <Link to="/" className="flex gap-3
+        items-center">
+          <BiHomeSmile className="text-white w-15 h-full"/>
+        </Link>
+        <Link to={`/profile/${userId}`} className="flex gap-3 items center">
+          <FaUserCircle className="text-white w-15 h-full"/>
+          <div className="flex flex-col">  
+            <p className="body-bold text-white">
+              {name} {lastName}
+            </p>
+            <p className="small-regular text-gray-600">
+              @{username}
+            </p>
+
+          </div>
+        </Link>
+      </div>  
+
+    </nav>
+  
   )
 }
 
