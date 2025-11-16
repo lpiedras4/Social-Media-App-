416a860f9f661faaadc1f21b5919230ce6f645fc
lpiedras4 <piedras.leo@gmail.com>
2025-11-15 23:06:43 -0800

    Fixed issue with tailwind version

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+export default {
+  theme: {
+    extend: {
+      colors: {
+        'dark-1': '#000000',
+        'dark-2': '#09090A',
+        'dark-3': '#101012',
+        'dark-4': '#1F1F22',
+        'light-1': '#FFFFFF',
+        'light-2': '#EFEFEF',
+        'light-3': '#7878A3',
+        'light-4': '#5C5C7B',
+        'primary-500': '#877EFF',
+        'red': '#FF5A5A',
+      },
+      screens: {
+        'xs': '480px',
+      },
+      width: {
+        '420': '420px',
+        '465': '465px',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+}
